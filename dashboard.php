60f62eb62c2100ced8844d416ab6da6eefb662ee
Bulanfebiola <bulanfebiola@gmail.com>
2026-01-17 10:40:43 +0700

    Membuat Tampilan Pelanggan

--- dashboard.php
+++ dashboard.php
@@ -102,7 +102,7 @@ session_start();
     <h2>Dashboard</h2>
     <a href="#">Home</a>
     <a href="?page=listproduk">List Produk</a>
-    <a href="#">Customer</a>
+    <a href="?page=pelanggan">Customer</a>
     <a href="#">Transaksi</a>
     <a href="#">Laporan</a>
   </div>
